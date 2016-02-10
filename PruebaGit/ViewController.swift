f1ca96384d1da41a37ff9b5f7bcf4b5e05f73202
Mariangeles Percastre <mariangelespercastre@macbook-air-de-mariangeles.local>
2016-02-10 00:10:51 -0600

    modi2

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    //modif 1
+    //modif 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
